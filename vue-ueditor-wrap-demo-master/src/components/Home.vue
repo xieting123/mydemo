e8acd4c31db7e1ee6fddea85d20406323aaf9345
谢婷 <1449708174@qq.com>
2019-09-17 10:05:15 +0800

    学习总结

---
+++ vue-ueditor-wrap-demo-master/src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <div @click="showData" class="preview" v-html="msg"></div>
+    <!-- 你不需要担心一个页面中过多vue-ueditor-wrap组件会导致混乱,或者重复引用JS(打开控制台瞄一眼),每个组件都保证拥有一个独立的UEditor实例,你可以尝试把数字改成99(根据自己电脑自行斟酌),但依然稳定可用 -->
+    <vue-ueditor-wrap ref="ueditor" v-model="msg" :destroy="false" :config="config" @ready="ready" ></vue-ueditor-wrap>
+  </div>
+</template>
+
+<script>
+// 1、引入VueUeditorWrap组件
+import VueUeditorWrap from 'vue-ueditor-wrap' // ES6 Module
+// const VueUeditorWrap = require('vue-ueditor-wrap') // CommonJS
+export default {
+  name: 'HelloWorld',
+  // 2、注册组件
+  components: {
+    VueUeditorWrap
+  },
+  data () {
+    return {
+      // 3、v-model绑定数据
+      msg: '<h2>1</h2>',
+      // 4、根据项目需求自行配置,具体配置参见ueditor.config.js源码或 http://fex.baidu.com/ueditor/#start-start
+      config: {
+        // 编辑器不自动被内容撑高
+        autoHeightEnabled: false,
+        // 初始容器高度
+        initialFrameHeight: 240,
+        // 初始容器宽度
+        initialFrameWidth: '100%',
+
+          serverUrl: 'https://www.ishangmi.cn/ueditor/config'
+
+      }
+    }
+  },
+  mounted () {
+    console.log('%c注意：本Demo提供的serverUrl是为了方便小伙伴们体验图片文件等上传功能。\n请勿在生产环境使用此serverUrl！！！', 'background:#f33;color:#fff')
+  },
+  methods: {
+    // 5、 你可以在ready方法中拿到editorInstance实例,所有API和官方的实例是一样了。http://fex.baidu.com/ueditor/#api-common
+    ready (editorInstance) {
+      console.log(`实例${editorInstance.key}已经初始化:`, editorInstance)
+    },
+    // 6. 查看绑定的数据
+    showData () {
+      alert(this.msg)
+      console.log(this.msg)
+    },
+    // 7. 借助 beforeInit 钩子，你可以实现对 UEditor 的二次开发，会在 scripts 加载完毕之后、编辑器初始化之前触发，以 编辑器 id 和 配置参数 作为入参
+   
+  }
+}
+</script>
+
+<style>
+.preview{
+  min-height: 150px;
+}
+</style>
---
+++ weapp-emoji/pages/demo/demo.js
@@ -0,0 +1,124 @@
+
+Page({
+  data: {
+    isShow: false,//控制emoji表情是否显示
+    isLoad: true,//解决初试加载时emoji动画执行一次
+    content: "",//评论框的内容
+    isLoading: true,//是否显示加载数据提示
+    disabled: true,
+    cfBg: false,
+    _index: 0,
+    comments:'',
+    emojiChar: "☺-😋-😌-😍-😏-😜-😝-😞-😔-😪-😭-😁-😂-😃-😅-😆-👿-😒-😓-😔-😏-😖-😘-😚-😒-😡-😢-😣-😤-😢-😨-😳-😵-😷-😸-😻-😼-😽-😾-😿-🙊-🙋-🙏-✈-🚇-🚃-🚌-🍄-🍅-🍆-🍇-🍈-🍉-🍑-🍒-🍓-🐔-🐶-🐷-👦-👧-👱-👩-👰-👨-👲-👳-💃-💄-💅-💆-💇-🌹-💑-💓-💘-🚲",
+    //0x1f---
+    emoji: [
+      "60a", "60b", "60c", "60d", "60f",
+      "61b", "61d", "61e", "61f",
+      "62a", "62c", "62e",
+      "602", "603", "605", "606", "608",
+      "612", "613", "614", "615", "616", "618", "619", "620", "621", "623", "624", "625", "627", "629", "633", "635", "637",
+      "63a", "63b", "63c", "63d", "63e", "63f",
+      "64a", "64b", "64f", "681",
+      "68a", "68b", "68c",
+      "344", "345", "346", "347", "348", "349", "351", "352", "353",
+      "414", "415", "416",
+      "466", "467", "468", "469", "470", "471", "472", "473",
+      "483", "484", "485", "486", "487", "490", "491", "493", "498", "6b4"
+    ],
+    emojis: [],//qq、微信原始表情
+    alipayEmoji: [],//支付宝表情
+  },
+ 
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var em = {}, that = this, emChar = that.data.emojiChar.split("-");
+    var emojis = []
+    that.data.emoji.forEach(function (v, i) {
+      em = {
+        char: emChar[i],
+        emoji: "0x1f" + v
+      };
+      emojis.push(em)
+    });
+       that.setData({
+        emojis: emojis
+      })
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+ 
+  //解决滑动穿透问题
+  emojiScroll: function (e) {
+    console.log(e)
+  },
+  //文本域失去焦点时 事件处理
+  textAreaBlur: function (e) {
+    //获取此时文本域值
+    console.log(e.detail.value,58)
+    this.setData({
+      content: e.detail.value
+    })
+
+  },
+  //文本域获得焦点事件处理
+  textAreaFocus: function () {
+    this.setData({
+      isShow: false,
+      cfBg: false
+    })
+  },
+  textAreaInput: function (e){
+    this.setData({
+      content: e.detail.value
+    })
+  },
+  //点击表情显示隐藏表情盒子
+  emojiShowHide: function () {
+    this.setData({
+      isShow: !this.data.isShow,
+      isLoad: false,
+      cfBg: !this.data.false
+    })
+  },
+  //表情选择
+  emojiChoose: function (e) {
+    //当前输入内容和表情合并
+    this.setData({
+      content: this.data.content + e.currentTarget.dataset.emoji
+    })
+  },
+  //点击emoji背景遮罩隐藏emoji盒子
+  cemojiCfBg: function () {
+    this.setData({
+      isShow: false,
+      cfBg: false
+    })
+  },
+  //发送评论评论 事件处理
+  send: function () {
+    var that = this, conArr = [];
+    //此处延迟的原因是 在点发送时 先执行失去文本焦点 再执行的send 事件 此时获取数据不正确 故手动延迟100毫秒
+    setTimeout(function () {
+      if (that.data.content.trim().length > 0) {
+        conArr.push({
+          avatar: util.ossAliyuncs + "/images/banner5.jpg",
+          uName: "测试",
+          time: util.formatTime(new Date()),
+          content: that.data.content
+        })
+        console.log(conArr)
+        that.setData({
+          comments: that.data.comments.concat(conArr),
+          content: "",//清空文本域值
+          isShow: false,
+          cfBg: false
+        })
+      } else {
+        that.setData({
+          content: ""//清空文本域值
+        })
+      }
+    }, 100)
+  }
+})
